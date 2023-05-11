e8e8d04c41cc0d58dd2cd87872ade556f868d181
Vishesh Singhal <visheshsinghal2001@gmail.com>
2023-05-12 01:51:51 +0530

    fixed carfactory methods to static

--- carFactory.py
+++ carFactory.py
@@ -11,17 +11,22 @@ from datetime import datetime
 
 class carFactory:
 
+    @staticmethod
     def create_calliope(current_date,service_date,current_mileage,last_service_mileage):
         return Car(CapuletEngine(current_mileage,last_service_mileage),splinder(current_date,service_date));
+    @staticmethod
     def create_Thovex(current_date,service_date,current_mileage,last_service_mileage):
         return Car(CapuletEngine(current_mileage,last_service_mileage),nubbin(current_date,service_date));
 
+    @staticmethod
     def create_rorschach(current_date,service_date,current_mileage,last_service_mileage):
         return Car(WilloughbyEngine(current_mileage,last_service_mileage),nubbin(current_date,service_date));
 
+    @staticmethod
     def create_glissade(current_date,service_date,current_mileage,last_service_mileage):
         return Car(WilloughbyEngine(current_mileage,last_service_mileage),splinder(current_date,service_date));
 
+    @staticmethod
     def create_pallindrome(current_date,service_date,warning_light_is_on):
         return Car(SternmanEngine(warning_light_is_on),splinder(current_date,service_date));
 
